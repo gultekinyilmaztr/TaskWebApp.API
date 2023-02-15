338bf7036f62d2a5adf2a4ee1322f98a229c2edf
Gültekin <gultekinyilmaz@hotmail.com.tr>
2023-02-15 21:31:15 +0300

    Add project files.

---
+++ TaskWebApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<IActionResult> GetTaskAsync()
+        {
+            // 5.Thread
+            var mytask = new HttpClient().GetStringAsync("https://www.google.com");
+
+            var data = await mytask;
+            return Ok(data);
+
+        }
+
+    }
+}
